cf5d2f9d896cd490544a81f6f6604de5f887169d
15931778201 <719791415@qq.com>
2021-03-28 21:11:50 +0800

    框架完善
    框架完善

--- src/http.js
+++ src/http.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const http = axios.create({
-  baseURL: "http://localhost:3000/admin/api"
+  baseURL: ProcessingInstruction.env.VUE_APP_AI_URL || "/admin/api",
+  // baseURL: "http://localhost:3000/admin/api"
 })
 
 export default http
